ad2257106036a357d5f58348a582a187caf0cb2a
Punvireakroth <vireakrothpun@gmail.com>
2022-07-11 22:39:08 +0700

    find amount of letter

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,17 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 
 // Function prototype
-
+int count_letters(string text);
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%s\n", text);
+    int letter = count_letters(text);
+    printf("%d\n", letter);
+
 }
 
 // count letter function
 int count_letters(string text)
 {
-
+    int amount_of_letter = 0;
+    int amount_of_word = 1;
+    int amount_of_sentence = 0;
+   for(int i = 0; i<= strlen(text); i++)
+   {
+    if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+    {
+        amount_of_letter++;
+    }
+   }
+   return amount_of_letter;
 }
