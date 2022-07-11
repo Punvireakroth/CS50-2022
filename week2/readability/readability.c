4c8b969bd512025e13444d72df06aa1556ec32fc
Punvireakroth <vireakrothpun@gmail.com>
2022-07-11 22:17:23 +0700

    initial commit

---
+++ week2/readability/readability.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+// Function prototype
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    printf("%s\n", text);
+}
+
+// count letter function
+int count_letters(string text)
+{
+
+}
